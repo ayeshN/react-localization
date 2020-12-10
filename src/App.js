7a2ca8b0f6731705e28fcfc6f5b8f89d844de69e
ayeshN <ayesh@ascentic.se>
2020-12-10 15:45:52 +0530

    Added t function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ const App = (props) => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{t("Hi")}</p>
+        <p>{t("Welcome")}</p>
       </header>
     </div>
   );
