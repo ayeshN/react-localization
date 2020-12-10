cdd2de462d275efea6426ec7a84227ef97de86e4
ayeshN <ayesh@ascentic.se>
2020-12-10 16:00:15 +0530

    Added language change function

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import { withTranslation } from "react-i18next";
 
 const App = (props) => {
   const { t } = props;
+
+  const onLanguageChanged = (event) => {
+    let language = event.currentTarget.value;
+    props.i18n.changeLanguage(language);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <button value="en" onClick={(value) => onLanguageChanged(value)}>
+          English
+        </button>
+        <button Value="sv" onClick={(value) => onLanguageChanged(value)}>
+          Swedish
+        </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>{t("Hi")}</p>
         <p>{t("Welcome")}</p>
